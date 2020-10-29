5dffebce3e903e065cadd03926715e7046c216c0
nukutka <snaffuxgreh@outlook.com>
2020-10-29 21:44:07 +0500

    rename project :)

---
+++ MyMessenger.Application.Contracts/ApplicationContractsModule.cs
@@ -0,0 +1,21 @@
+﻿using MyMessenger.Core;
+using Volo.Abp.AutoMapper;
+using Volo.Abp.Modularity;
+
+namespace MyMessenger.Application.Contracts
+{
+    [DependsOn(
+        typeof(CoreModule),
+        typeof(AbpAutoMapperModule)
+        )]
+    public class ApplicationContractsModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpAutoMapperOptions>(options =>
+            {
+                options.AddMaps<ApplicationContractsModule>();
+            });
+        }
+    }
+}
---
+++ MyMessenger.Application.Contracts/ControllerRequests/ChatController/UpdateChatInput.cs
@@ -0,0 +1,15 @@
+﻿using MyMessenger.Application.Contracts.DTOs.Messaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyMessenger.Application.Contracts.ControllerRequests.ChatController
+{
+    public class UpdateChatInput
+    {
+        [Required]
+        public ChatDtoInput Chat { get; set; }
+
+        public List<Guid> UserIds { get; set; }
+    }
+}
