db99e57818b21536d34bd086690a4dc058188567
nukutka <snaffuxgreh@outlook.com>
2020-11-12 00:24:11 +0500

    basic components structure

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -2,15 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MaterialModule} from './material.module';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { MainComponent } from './components/main.component';
+import { LeftBarComponent } from './components/left-bar/left-bar.component';
+import { ContactListComponent } from './components/left-bar/contact-list/contact-list.component';
+import { ChatComponent } from './components/chat/chat.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    MainComponent,
+    LeftBarComponent,
+    ContactListComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: MainComponent },
+      // { path: 'login', component: AuthComponent },
+      { path: '**', redirectTo: '/'}
+    ]),
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
